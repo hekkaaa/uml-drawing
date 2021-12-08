a9c94d9cf60600b4a2eae5a759ee07f80f8672a7
Alexander Pototsky <silencemyalise@gmail.com>
2021-12-08 13:27:21 +0300

    Add Project UnitTest Framefork 4.7

---
+++ UML_drawing/TestProject/UnitTestDB.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TestProject
+{
+    public class UnitTestDB
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
